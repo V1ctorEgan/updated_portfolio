73616cc97e24eac56684cf748fa20ec40d09c496
victor egan <victorifeanyi2004@gmail.com>
2025-07-08 12:03:23 +0100

    removed ball

--- src/components/largeLogo.jsx
+++ src/components/largeLogo.jsx
@@ -1,6 +1,6 @@
 const LargeLogo = () =>{
     return(
-        <div className="w-[160px] h-[160px] bg-green-500 rounded-[80px] mt-12 animate-bounce">
+        <div className="w-[160px] h-[160px] bg-green-500 rounded-[80px] mt-12 ">
 
         </div>
     )
